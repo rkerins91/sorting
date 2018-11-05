ed8eb2f87bc598f74073541ac335254626152eec
Pavel Machuca <pavelmachuca@hotmail.com>
2018-11-05 11:41:34 -0500

    it runs?

--- bubblesort.js
+++ bubblesort.js
@@ -1,1 +1,40 @@
 /* bubble sort */
+
+function bubbleSort(array) {
+  let sorting = true,
+    checkLength = array.length - 1,
+    counter = 0;
+
+  while (sorting) {
+    counter++;
+    sorting = false;
+    for (let i = 0; i < checkLength; i++) {
+      let temp;
+      if (array[i] > array[i + 1]) {
+        temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        sorting = true;
+      }
+    }
+    checkLength--;
+  }
+
+  console.log("counted :: ", counter);
+  return array;
+}
+
+// function swap(numOne, numTwo) {
+// if(numOne > numTwo) {
+
+// } else {
+
+// }
+
+//   return numOne, numTwo;
+// }
+
+let input = [9, 8, 11, 12, 13, 14, 15, 16];
+
+console.log("unsorted :: ", input);
+console.log(bubbleSort(input));
---
+++ bubblesort.spec.js
@@ -0,0 +1,22 @@
+// /*
+//   specs to do
+//   1. input array and return sorted
+//   2. still sorts for repeated
+//   3.
+// */
+
+// describe("Bubble Sort", function() {
+//   it("handles an empty array", function() {
+//     expect(bubbleSort([])).toEqual([]);
+//   });
+
+//   it("returns sorted array for single item", function() {
+//     expect(bubbleSort([6])).toEqual([6]);
+//   });
+//   it("returns sorted array for multiple items", function() {
+//     let input = [6, 5, 9, 13, 8, 84, 65],
+//       answer = [5, 6, 8, 9, 13, 65, 84];
+
+//     expect(bubbleSort(input)).toEqual(answer);
+//   });
+// });
